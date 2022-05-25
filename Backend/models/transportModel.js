924c6e6c6c6ed4d7a31c9c4a629fe9b15799b67c
it18229608 <It18229608@my.sliit.lk>
2022-05-25 16:42:52 +0530

    transport models Added

---
+++ Backend/models/transportModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const transportSchema = new Schema({
+  Name: {
+    type: String,
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+  },
+  Appoinmentid: {
+    type: String,
+    required: true,
+  },
+  Location: {
+    type: String,
+    required: true,
+  },
+  Comments: {
+    type: String,
+    required: true,
+  },
+});
+const transport = mongoose.model("Transport", transportSchema);
+module.exports = transport;
