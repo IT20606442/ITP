ecd1ef121126b3073170c2e4f0a60930be288986
IT20606442 <it20606442@my.sliit.lk>
2022-05-25 16:14:33 +0530

    comit

--- Backend/models/appointmentModel.js
+++ Backend/models/appointmentModel.js
@@ -1,1 +1,42 @@
-//hjkrfrt
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const appointmentSchema = new Schema({
+  ID: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+
+  name: {
+    type: String,
+    required: true,
+  },
+
+  age: {
+    type: String,
+    required: true,
+  },
+
+  tpNo: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  reason: {
+    type: String,
+    required: true,
+  },
+});
+
+const Appointment = mongoose.model("Appointment", appointmentSchema);
+
+module.exports = Appointment;
